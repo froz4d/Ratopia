2cd128c7c402d02442a5c27a2b8316c47ef77ffb
phiraphonnn <65663938+phiraphonnn@users.noreply.github.com>
2023-10-17 03:14:08 +0700

    kuy update

--- Assets/NewPlayerController.cs
+++ Assets/NewPlayerController.cs
@@ -5,25 +5,28 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class NewPlayerController : MonoBehaviour,IDragHandler,IBeginDragHandler,IEndDragHandler
+public class NewPlayerController : MonoBehaviour, IDragHandler, IBeginDragHandler, IEndDragHandler
 {
     private Vector3 _initialPosition;
-    private Vector3 leftinfo = new Vector3(140,0,0);
-    private Vector3 Rightinfo = new Vector3(-140,0,0);
-   private Vector3 _initialRotation ;
+    private Vector3 leftinfo = new Vector3(140, 0, 0);
+    private Vector3 Rightinfo = new Vector3(-140, 0, 0);
+    private Vector3 _initialRotation;
     private float _distanceMoved;
     private bool _swipeLeft;
+    private StateCard currenState = StateCard.None;
 
     public void OnDrag(PointerEventData eventData)
     {
-        transform.localPosition = new Vector2(transform.localPosition.x+eventData.delta.x,transform.localPosition.y);
+        transform.localPosition = new Vector2(transform.localPosition.x + eventData.delta.x, transform.localPosition.y);
         if (transform.localPosition.x - _initialPosition.x > 0)
         {
-            transform.localEulerAngles = new Vector3(0, 0, Mathf.LerpAngle(0, -10, (_initialPosition.x + transform.localPosition.x) / (Screen.width / 2)));
+            transform.localEulerAngles = new Vector3(0, 0,
+                Mathf.LerpAngle(0, -10, (_initialPosition.x + transform.localPosition.x) / (Screen.width / 2)));
         }
         else
         {
-            transform.localEulerAngles = new Vector3(0, 0, Mathf.LerpAngle(0, 10, (_initialPosition.x - transform.localPosition.x) / (Screen.width / 2)));
+            transform.localEulerAngles = new Vector3(0, 0,
+                Mathf.LerpAngle(0, 10, (_initialPosition.x - transform.localPosition.x) / (Screen.width / 2)));
         }
     }
 
@@ -31,15 +34,15 @@ public class NewPlayerController : MonoBehaviour,IDragHandler,IBeginDragHandler,
     {
         _initialPosition = transform.localPosition;
         _initialRotation = transform.localEulerAngles;
-        
+
     }
 
     public void OnEndDrag(PointerEventData eventData)
     {
         _distanceMoved = Mathf.Abs(transform.localPosition.x - _initialPosition.x);
-        if(_distanceMoved<0.4*Screen.width)
+        if (_distanceMoved < 0.4 * Screen.width)
         {
-            StartCoroutine(ReturnToInitialPosition());
+            currenState = StateCard.ReturnCard;
         }
         else
         {
@@ -51,7 +54,8 @@ public class NewPlayerController : MonoBehaviour,IDragHandler,IBeginDragHandler,
             {
                 _swipeLeft = true;
             }
-            StartCoroutine(MovedCard());
+
+            currenState = StateCard.CardMove;
         }
     }
 
@@ -75,16 +79,65 @@ public class NewPlayerController : MonoBehaviour,IDragHandler,IBeginDragHandler,
                 GetComponent<Image>().color = Color.Lerp(Color.white, Color.green, Mathf.SmoothStep(1, 0, 4 * time));
             }
 
-          //  GetComponent<Image>().color = new Color(1, 1, 1, Mathf.SmoothStep(1, 0, 4 * time));
+            //  GetComponent<Image>().color = new Color(1, 1, 1, Mathf.SmoothStep(1, 0, 4 * time));
             yield return null;
         }
 
         Destroy(gameObject);
     }
 
-    private IEnumerator ReturnToInitialPosition()
+    private void MoveCard()
+    {
+        float time = 0;
+        while (time < 0.2f)
+        {
+            time += Time.deltaTime;
+
+            if (_swipeLeft)
+            {
+                transform.localPosition = new Vector3(Mathf.SmoothStep(transform.localPosition.x,
+                    transform.localPosition.x - Screen.width, time), transform.localPosition.y, 0);
+                GetComponent<Image>().color = Color.Lerp(Color.white, Color.red, Mathf.SmoothStep(1, 0, 4 * time));
+            }
+            else
+            {
+                transform.localPosition = new Vector3(Mathf.SmoothStep(transform.localPosition.x,
+                    transform.localPosition.x + Screen.width, time), transform.localPosition.y, 0);
+                GetComponent<Image>().color = Color.Lerp(Color.white, Color.green, Mathf.SmoothStep(1, 0, 4 * time));
+            }
+
+            //  GetComponent<Image>().color = new Color(1, 1, 1, Mathf.SmoothStep(1, 0, 4 * time));
+
+        }
+
+        Destroy(gameObject);
+
+    }
+
+    private void ReturnCardToPosition()
     {
         float duration = 0.5f; // Adjust the duration as needed for the desired smoothness.
+        float elapsedTime = 0f;
+
+        Quaternion initialRotation = Quaternion.Euler(_initialRotation);
+
+        while (elapsedTime < duration)
+        {
+            transform.localPosition = Vector3.Slerp(transform.localPosition, new Vector3(0,0,0), elapsedTime / duration);
+            transform.localRotation = Quaternion.Slerp(transform.localRotation, initialRotation, elapsedTime / duration);
+
+            elapsedTime += Time.deltaTime;
+        }
+
+        // Ensure the card ends up exactly at the initial position and rotation.
+        transform.localPosition = new Vector3(0,0,0);
+        transform.localRotation = initialRotation;
+        currenState = StateCard.None;
+    }
+
+    private IEnumerator ReturnToInitialPosition()
+    {
+        float duration = 0.2f; // Adjust the duration as needed for the desired smoothness.
         float elapsedTime = 0f;
 
         Quaternion initialRotation = Quaternion.Euler(_initialRotation);
@@ -113,5 +166,24 @@ public class NewPlayerController : MonoBehaviour,IDragHandler,IBeginDragHandler,
         {
             Debug.LogWarning("Show info Left");
         }
+
+        switch (currenState)
+        {
+            case StateCard.None:
+                
+                break;
+            case StateCard.CardMove:
+                MoveCard();
+                break;
+            case StateCard.ReturnCard:
+                ReturnCardToPosition();
+                break;
+        }
+    }
+    private enum StateCard
+    {
+        None,
+        CardMove,
+        ReturnCard
     }
 }
