5d92fd04866a7ceca93e3e39e5b435f16d59a056
FrozenTEAR <yotsaphat.laph@bumail.net>
2023-10-31 02:15:00 +0700

    ระบบ Audio
    Import some raw Music Background

---
+++ Assets/AudioManager.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public List<AudioClip> playlist;
+    private AudioSource audioSource;
+
+    void Start()
+    {
+        audioSource = this.gameObject.GetComponent<AudioSource>();
+
+        if (playlist.Count == 0)
+        {
+            Debug.LogError("No audio clips in the playlist. Please add audio clips in the Inspector.");
+            enabled = false; // Disable the script
+        }
+
+        // Play random track from the playlist
+        PlayRandomTrack();
+
+        // Play the background music
+        audioSource.Play();
+    }
+
+    void PlayRandomTrack()
+    {
+        int randomIndex = Random.Range(0, playlist.Count); // Choose a random index
+        audioSource.clip = playlist[randomIndex]; // Set the chosen clip to the AudioSource
+        audioSource.Play(); // Play the audio clip
+
+        // Subscribe to the AudioSource's "ended" event to play the next random track when the current one ends
+        audioSource.loop = false; // Disable the built-in loop
+        audioSource.PlayOneShot(playlist[randomIndex]);
+    }
+
+    void Update()
+    {
+        // Check if the current track has ended
+        if (!audioSource.isPlaying)
+        {
+            PlayRandomTrack();
+        }
+    }
+}
