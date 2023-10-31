0ada28df350accfb4d02c4b686d913ac472f510e
phiraphonnn <65663938+phiraphonnn@users.noreply.github.com>
2023-11-01 02:45:40 +0700

    RemoteConfig

--- Assets/EventConfig.cs
+++ Assets/EventConfig.cs
@@ -1,18 +1,55 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Unity.Services.RemoteConfig;
 
 public class EventConfig : MonoBehaviour
 {
+
+    [SerializeField] private GameObject DefaultCard;
+    [SerializeField] private GameObject HalloweenCard;
+    public struct userAttributes { }
+    public struct appAttributes { }
+
+    public bool IsHalloween;
     // Start is called before the first frame update
-    void Start()
+    IEnumerator Start()
     {
-        
+        while (true)
+        {
+            yield return new WaitForSeconds(3);
+            RemoteConfigService.Instance.FetchCompleted += ApplyHalloween;
+            RemoteConfigService.Instance.FetchConfigs<userAttributes,appAttributes>(new userAttributes(),new appAttributes());
+        }
     }
 
+    void ApplyHalloween(ConfigResponse configResponse)
+    {
+        IsHalloween = RemoteConfigService.Instance.appConfig.GetBool("IsHalloween");
+        if (IsHalloween)
+        {
+            setuphalloween();
+        }
+        else
+        {
+            setupDefault();
+        }
+    }
+
+    void setuphalloween()
+    {
+        HalloweenCard.SetActive(true);
+        DefaultCard.SetActive(false);
+    }
+    void setupDefault()
+    {
+        HalloweenCard.SetActive(false);
+        DefaultCard.SetActive(true);
+    }
     // Update is called once per frame
     void Update()
     {
         
     }
+    
 }
