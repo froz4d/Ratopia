730a40898c44016d52913394944b5726f72c390b
phiraphonnn <65663938+phiraphonnn@users.noreply.github.com>
2023-11-15 01:30:56 +0700

    boot fps?

--- Assets/FPSDisplay.cs
+++ Assets/FPSDisplay.cs
@@ -3,7 +3,10 @@ using UnityEngine;
 public class FPSDisplay : MonoBehaviour
 {
     float deltaTime = 0.0f;
-
+    void Start()
+    {
+        Application.targetFrameRate = 120;
+    }
     void Update()
     {
         deltaTime += (Time.unscaledDeltaTime - deltaTime) * 0.1f;
